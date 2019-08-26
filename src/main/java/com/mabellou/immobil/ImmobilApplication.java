36a6e23a99433745bb88c8bd190638d39408b235
mabellou <marwan.bellouti@5thfloor.be>
2019-08-26 22:00:58 +0200

    first commit

---
+++ src/main/java/com/mabellou/immobil/ImmobilApplication.java
@@ -0,0 +1,13 @@
+package com.mabellou.immobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobilApplication.class, args);
+	}
+
+}
